43b42b1b8fc2f534e9c154f0be07649e65fcc9f8
akashdgo <akashd.go@yandex.ru>
2020-07-15 18:21:46 +0530

    Commit package excercise and tutorial example

--- 06-library-packages/01-exporting/excercise/ver/ver.go
+++ 06-library-packages/01-exporting/excercise/ver/ver.go
@@ -2,7 +2,7 @@ package ver
 
 import "runtime"
 
-// Version prints 'go version' as a string
+// Version returns value of 'go version' as a string
 func Version() string {
 	return runtime.Version()
 }
